64bdd187bf2ba25634347e2d777458ec27c3b462
Michael Doggett <michael.doggett@cs.lth.se>
2023-05-26 10:39:36 +0200

    first commit

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({ color:0xff0000 })
+const mesh     = new THREE.Mesh(geometry,material)
+
+scene.add(mesh)
+
+// Sizes
+const sizes = {    width:window.innerWidth,     height:window.innerHeight }
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height,0.1,100)
+camera.position.z = 3
+scene.add(camera)
+
+const renderer = new THREE.WebGLRenderer()
+
+renderer.setSize(sizes.width,sizes.height)
+document.body.appendChild(renderer.domElement);
+
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene,camera)
+}
+animate()
